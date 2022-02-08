f262d496990bb3b438023053f34ca26eabb71631
Plamen Petkov <plamen.petkov@atscale.com>
2022-02-08 10:22:05 +0200

    update

--- src/components/GoogleForm.js
+++ src/components/GoogleForm.js
@@ -5,7 +5,7 @@ const GoogleForm = () => {
         <section className="section w-details-area center-text">
             <div className="container">
                 <div className="heading">
-                    <h4>Моля потвърдете своето присъствие като попълните анкетата!</h4>
+                    <h4>Моля потвърдете своето присъствие като попълните анкетата до 30 април!</h4>
                 </div>
                 <a href="https://forms.gle/PqPG9siZdNc8nSaA8"><span className="form">Към Анкетата!</span></a>
                 <div className="heading">
